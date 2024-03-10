066b14e6a0a029eaf7ad6549b57469be1242a2c6
Raudin <markmuga72@gmail.com>
2024-03-10 23:49:07 +0300

    Update on ticketbooking migration

--- ticket-booking/database/migrations/2024_02_29_193026_add_roles_to_users_table.php
+++ ticket-booking/database/migrations/2024_02_29_193026_add_roles_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')->default(2)->constrained()->cascadeOnDelete();
         });
     }
 
